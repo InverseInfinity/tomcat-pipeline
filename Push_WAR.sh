e941bb385e8e09d27c0faee347a74de6a7b270e7
InverseInfinity <johappyalways@gmail.com>
2022-02-13 12:06:56 +0000

    updated

--- Push_WAR.sh
+++ Push_WAR.sh
@@ -4,4 +4,4 @@ echo ********************************************************
 echo **** Pushing artifact -WAR file to Tomcat server  ******
 echo ********************************************************
  
-scp -i /var/lib/jenkins/remote /var/lib/jenkins/workspace/tomcat_pipline/webapp/target/*.war ec2-user@172.31.30.85:~/
+scp -i /var/lib/jenkins/remote /var/lib/jenkins/workspace/tomcat_pipline/webapp/target/*.war ec2-user@$AWS_IP:~/
