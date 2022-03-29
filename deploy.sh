6e4b7f8a34ffef1fdf6e1c20c2a13c5aca2c7140
Nav_InverseInfinity <98486154+nav-InverseInfinity@users.noreply.github.com>
2022-03-29 18:20:07 +0100

    updated

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 docker-compose -f docker-compose.yml up -d
 
-echo ****** Copying WAR file to tomcat webapp directory *********
+echo " ****** Copying WAR file to tomcat webapp directory ********* "
 
 docker cp /home/ec2-user/*.war tomcat:/usr/local/tomcat/webapps/
 
